b36526c79c70fdb0483deddd7d88df3e9bb18d12
NeilH <neilhemm0@gmail.com>
2022-12-18 19:44:13 -0500

    Create basic functions for add, sub, mult, div

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //Logic for the calculator 
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
