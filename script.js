a6da2bd6ba9e711e5efc93748448272a2369caad
NeilH <neilhemm0@gmail.com>
2022-12-18 20:04:42 -0500

    Add operate function and test all functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//Logic for the calculator 
+//Logic for the calculator
+
+let runningProd = 0;
 
 function add(a, b) {
     return a + b;
@@ -15,3 +17,14 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, opp, b) {
+    return opp(a, b);
+}
+
+console.log(add(20, 2));
+console.log(subtract(20, 2));
+console.log(multiply(20, 2));
+console.log(divide(20, 2));
+
+console.log(operate(20, add, 2));
